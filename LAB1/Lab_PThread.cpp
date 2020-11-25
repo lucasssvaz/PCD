ec8b197466988c5eb950a9560469d2fe0c28fd1e
Lucas Vaz <lucassvaz@yahoo.com.br>
2020-11-25 19:45:09 -0300

    Implemented PThread Version (Testing Req.)

--- LAB1/Lab_PThread.cpp
+++ LAB1/Lab_PThread.cpp
@@ -6,6 +6,7 @@
 
 #define SRAND_VALUE 1985
 #define N_THREADS 4
+#define N_GENERATIONS 2000
 
 //#define DEBUG
 
@@ -17,9 +18,16 @@
 
 using namespace std;
 
+struct PT_Args
+{
+	vector<vector<bool>>* Grid;
+	vector<vector<bool>>* New_Grid;
+	int X, Y;
+};
+
 //==================================================================================================================================
 
-void Grid_Init (vector< vector<bool> > &Grid)
+void Grid_Init(vector<vector<bool>> &Grid)
 {
 	srand(SRAND_VALUE);
 
@@ -30,10 +38,10 @@ void Grid_Init (vector< vector<bool> > &Grid)
 
 //--------------------------------------------------------------------------
 
-int Neighbours_Count (vector< vector<bool> > &Grid, int X, int Y)
+int Neighbours_Count(vector<vector<bool>> &Grid, int X, int Y)
 {
 	int Nb_Total = 0;													//Qnt. of Neighbours
-	vector < pair <int, int> > Nb_List;									//Vector of Neighbours coordinates for future validation
+	vector <pair <int, int>> Nb_List;									//Vector of Neighbours coordinates for future validation
 
 	for(int i = X-1; i <= X+1; i++) 									//Analyses all squares around current X,Y
 		for(int j = Y-1; j <= Y+1; j++)
@@ -63,7 +71,7 @@ int Neighbours_Count (vector< vector<bool> > &Grid, int X, int Y)
 
 //--------------------------------------------------------------------------
 
-int Cells_Total (vector< vector<bool> > &Grid)
+int Cells_Total(vector<vector<bool>> &Grid)
 {
 	int Cells_Sum = 0;
 
@@ -75,20 +83,101 @@ int Cells_Total (vector< vector<bool> > &Grid)
 	return Cells_Sum;
 }
 
+//--------------------------------------------------------------------------
+
+void *Cell_Update(void *Args)
+{
+	vector<vector<bool>> *Grid, *New_Grid;
+	int X, Y;
+
+	Grid = ((struct PT_Args*) Args)->Grid;
+	New_Grid = ((struct PT_Args*) Args)->New_Grid;						//Extracts data from Args Struct
+	X = ((struct PT_Args*) Args)->X;
+	Y = ((struct PT_Args*) Args)->Y;
+
+	int Nb_Count = Neighbours_Count(*Grid, X, Y);
+
+	if ((*Grid)[X][Y] == 0)
+	{
+		if (Nb_Count == 3)
+		{
+			(*New_Grid)[X][Y] = 1;
+			free(Args);
+			pthread_exit(NULL);
+		}
+	}
+	else
+	{
+		if (Nb_Count < 2 || Nb_Count >= 4)
+		{
+			(*New_Grid)[X][Y] = 0;
+			free(Args);
+			pthread_exit(NULL);
+		}		
+	}
+
+	(*New_Grid)[X][Y] = (*Grid)[X][Y];
+
+	free(Args);
+	pthread_exit(NULL);
+}
+
+//--------------------------------------------------------------------------
+
+void Grid_Update(vector<vector<bool>> &Grid, vector<vector<bool>> &New_Grid)
+{
+	pthread_t Threads[N_THREADS];
+	int X, Y;
+	int PT_Count = 0;
+	struct PT_Args *Args;
+
+
+	for (X = 0; X < (int)Grid.size(); X++)
+	{
+		for (Y = 0; Y < (int)Grid.size(); Y++)
+		{
+			if (PT_Count % N_THREADS == 0)
+				for (int i = 0; i < N_THREADS; i++)
+					pthread_join(Threads[i], NULL);
+
+			Args = (struct PT_Args*) malloc(sizeof(struct PT_Args));
+			Args->X = X;
+			Args->Y = Y;
+			Args->Grid = &Grid;
+			Args->New_Grid = &New_Grid;
+
+			pthread_create(&Threads[PT_Count % N_THREADS], NULL, Cell_Update, (void*) Args);
+
+			PT_Count++;
+		}
+	}
+
+	for (int i = 0; i < N_THREADS; i++)
+		pthread_join(Threads[i], NULL);
+}
+
 //==================================================================================================================================
 
 int main()
 {
 	int N;
-	pthread_t Threads[N_THREADS];
 
 	cout << "Enter the desired matrix order:" << endl;
 	cin >> N;
 
-	vector< vector<bool> > Grid (N, vector<bool> (N));
-	vector< vector<bool> > New_Grid (N, vector<bool> (N));
+	vector<vector<bool>> Grid (N, vector<bool> (N));
+	vector<vector<bool>> New_Grid (N, vector<bool> (N));
 
 	Grid_Init(Grid);
+
+	cout << "Generation 0: " << Cells_Total(Grid) << endl; 
+
+	for (int i = 0; i < N_GENERATIONS; i++)
+	{
+		Grid_Update(Grid, New_Grid);
+		Grid = New_Grid;
+		cout << "Generation " << i << ": " << Cells_Total(Grid) << endl;
+	}
 
 	return 0;
 }
